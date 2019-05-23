b8543eab6cfdb8d9b32804ae596d0691099bc5ab
nadger <nick@nadger.com>
2019-05-23 17:14:14 +0100

    update 3

--- entries/admin.py
+++ entries/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Fixtures, Teams
+from .models import Fixtures, Teams, Question
 
 admin.site.register(Fixtures)
 admin.site.register(Teams)
+admin.site.register(Question)
--- entries/models.py
+++ entries/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
+ #answer = models.PositiveIntegerField(default=0)
 # Create your models here.
 class Teams(models.Model):
 	team_name = models.CharField(max_length=100)
 	def __str__(self):
     		return self.team_name
-
-
 class Fixtures(models.Model):
 	id = models.AutoField(primary_key=True)
 	home_team = models.ForeignKey(Teams, models.CASCADE, related_name="games_as_team1", null=True)
@@ -14,16 +12,28 @@ class Fixtures(models.Model):
 	game_week = models.PositiveIntegerField(default=0)
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    game_week = models.IntegerField
-    answer = models.IntegerField
-
+    #TRUE = 'TRUE'
+    #FALSE = 'FALSE'
+    #Q_CHOICES = [(TRUE, 'True'), (FALSE, 'False')]
+	#answer = models.CharField(max_length=10, choices=Q_CHOICES, default=TRUE
+	#question_text = models.CharField(max_length=200)
+    #game_week = models.PositiveIntegerField(default=0)
+    TRUE = 'TRUE'
+    FALSE = 'FASLE'
+    Q_CHOICES = [
+        (TRUE, 'True'),
+        (FALSE, 'False'),
+    ]
+    answer = models.CharField(
+        max_length=5,
+        choices=Q_CHOICES,
+        default=TRUE,
+    )
+    question_text = models.CharField(max_length=200, default="test")
+    game_week = models.PositiveIntegerField(default=0)
+	
 class Players(models.Model):
 	player_fname = models.CharField(max_length=100)
 	player_lname = models.CharField(max_length=100)
 	player_team = models.ForeignKey(Teams, on_delete=models.CASCADE)
-
-#class Results(models.Model)
-#	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	
 		
